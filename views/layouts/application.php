ddaecf9737cd3d8e803e79f86a0541b8bbc82a8a
TranKhacThien <55495220+TranKhacThien@users.noreply.github.com>
2020-04-21 08:50:39 +0700

    Add files via upload

--- views/layouts/application.php
+++ views/layouts/application.php
@@ -27,8 +27,9 @@
 
 
 <!-- Bootstrap core JavaScript -->
-<script src="https://onlinemedicalmanagementsystem.herokuapp.com/vendor/jquery/jquery.min.js"></script>
-<script src="https://onlinemedicalmanagementsystem.herokuapp.com/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js" integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm" crossorigin="anonymous"></script>
 
 <!-- Menu Toggle Script -->
 <script>
