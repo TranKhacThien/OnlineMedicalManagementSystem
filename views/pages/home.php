3924ea1cbb44b3f8517b7748aa8c4dd8e8c9d4e1
TranKhacThien <55495220+TranKhacThien@users.noreply.github.com>
2020-04-22 20:09:51 +0700

    Add files via upload

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,70 +1,71 @@
-<div class="d-flex" id="wrapper">
-
-
-    <div class="bg-light border-right" id="sidebar-wrapper">
-        <div class="sidebar-heading"> <a href="" class="text-dark page-item"><img src="image/home.png" title="home" width="200" height="100"></a>  </div>
-        <div class="list-group list-group-flush">
-            <li class="list-group-item bg-light">
-                <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle  list-group-item-action bg-light text-dark">Giới thiệu</a>
-                <ul class="collapse list-unstyled" id="homeSubmenu">
-                    <a href="#" class="list-group-item list-group-item-action bg-light">Cơ sở vật chất</a>
-                    <a href="#" class="list-group-item list-group-item-action bg-light">Đội ngũ y tế</a>
-                </ul>
-            </li>
-            <a href="#" class="list-group-item list-group-item-action bg-light">Tin tức</a>
-            <a href="#" class="list-group-item list-group-item-action bg-light">Đội ngũ y tế</a>
-            <a href="#" class="list-group-item list-group-item-action bg-light">Đặt lịch</a>
-            <a href="#" class="list-group-item list-group-item-action bg-light">Thông tin cá nhân</a>
-            <a href="#" class="list-group-item list-group-item-action bg-light">Hỏi đáp</a>
-            <a href="#" class="list-group-item list-group-item-action bg-light">Báo cáo</a>
-        </div>
-
-    </div>
-
-    <div id="page-content-wrapper">
-        <header>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
-                <button class="btn btn-primary" id="menu-toggle">Menu</button>
-
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="index.php?controller=pages&action=home">Trang chủ<span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Liên hệ
-                            </a>
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="#">0909587999</a>
-
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="#">Địa chỉ </a>
-                            </div>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Tài khoản
-                            </a>
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="#">Thông tin tài khoản</a>
-
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="index.php?controller=login&action=logout">Đăng xuất</a>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </header>
-
-
-
-
+<!--<div class="d-flex" id="wrapper">-->
+<!---->
+<!---->
+<!--    <div class="bg-light border-right" id="sidebar-wrapper">-->
+<!--        <div class="sidebar-heading"> <a href="" class="text-dark page-item"><img src="image/home.png" title="home" width="200" height="100"></a>  </div>-->
+<!--        <div class="list-group list-group-flush">-->
+<!--            <li class="list-group-item bg-light">-->
+<!--                <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle  list-group-item-action bg-light text-dark">Giới thiệu</a>-->
+<!--                <ul class="collapse list-unstyled" id="homeSubmenu">-->
+<!--                    <a href="#" class="list-group-item list-group-item-action bg-light">Cơ sở vật chất</a>-->
+<!--                    <a href="#" class="list-group-item list-group-item-action bg-light">Đội ngũ y tế</a>-->
+<!--                </ul>-->
+<!--            </li>-->
+<!--            <a href="#" class="list-group-item list-group-item-action bg-light">Tin tức</a>-->
+<!--            <a href="#" class="list-group-item list-group-item-action bg-light">Đội ngũ y tế</a>-->
+<!--            <a href="#" class="list-group-item list-group-item-action bg-light">Đặt lịch</a>-->
+<!--            <a href="#" class="list-group-item list-group-item-action bg-light">Thông tin cá nhân</a>-->
+<!--            <a href="#" class="list-group-item list-group-item-action bg-light">Hỏi đáp</a>-->
+<!--            <a href="#" class="list-group-item list-group-item-action bg-light">Báo cáo</a>-->
+<!--        </div>-->
+<!---->
+<!--    </div>-->
+<!---->
+<!--    <div id="page-content-wrapper">-->
+<!--        <header>-->
+<!--            <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">-->
+<!--                <button class="btn btn-primary" id="menu-toggle">Menu</button>-->
+<!---->
+<!--                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">-->
+<!--                    <span class="navbar-toggler-icon"></span>-->
+<!--                </button>-->
+<!---->
+<!--                <div class="collapse navbar-collapse" id="navbarSupportedContent">-->
+<!--                    <ul class="navbar-nav ml-auto mt-2 mt-lg-0">-->
+<!--                        <li class="nav-item active">-->
+<!--                            <a class="nav-link" href="index.php?controller=pages&action=home">Trang chủ<span class="sr-only">(current)</span></a>-->
+<!--                        </li>-->
+<!--                        <li class="nav-item dropdown">-->
+<!--                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">-->
+<!--                                Liên hệ-->
+<!---->
+<!--                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">-->
+<!--                                <a class="dropdown-item" href="#">0909587999</a>-->
+<!---->
+<!--                                <div class="dropdown-divider"></div>-->
+<!--                                <a class="dropdown-item" href="#">Địa chỉ </a>-->
+<!--                            </div>-->
+<!--                            </a>-->
+<!--                        </li>-->
+<!--                        <li class="nav-item dropdown">-->
+<!--                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">-->
+<!--                                Tài khoản-->
+<!--                            </a>-->
+<!--                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">-->
+<!--                                <a class="dropdown-item" href="#">Thông tin tài khoản</a>-->
+<!---->
+<!--                                <div class="dropdown-divider"></div>-->
+<!--                                <a class="dropdown-item" href="index.php?controller=login&action=logout">Đăng xuất</a>-->
+<!--                            </div>-->
+<!--                        </li>-->
+<!--                    </ul>-->
+<!--                </div>-->
+<!--            </nav>-->
+<!--        </header>-->
+<!---->
+<!---->
+<!---->
+<!---->
 
         <div class="container">
 
@@ -232,8 +233,8 @@
         </div>
 
 
-    </div>
+<!--    </div>-->
 
 
-</div>
+<!--</div>-->
 
