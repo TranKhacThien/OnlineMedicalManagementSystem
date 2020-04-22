f8f75d24a0e4334efc662e6c05e8549b367a8ce1
TranKhacThien <55495220+TranKhacThien@users.noreply.github.com>
2020-04-22 22:20:09 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -17,15 +17,3 @@ class DB
     }
 }
 
-
-// $host="localhost";
-// $port=3306;
-// $socket="";
-// $user="root";
-// $password="";
-// $dbname="classicmodels";
-
-// $con = new mysqli($host, $user, $password, $dbname, $port, $socket)
-// 	or die ('Could not connect to the database server' . mysqli_connect_error());
-
-//$con->close();
