ab0007f8109e12025134327159297ebfec7bfc23
TranKhacThien <55495220+TranKhacThien@users.noreply.github.com>
2020-06-14 18:07:04 +0700

    .

--- views/pages/report.php
+++ views/pages/report.php
@@ -8,8 +8,8 @@
                             echo "Bạn không có lịch khám gần đây</br>";
                         else{
                     ?>
-                <label for="bookingID" class="text-dark font-weight-bold">Lịch khám:</label><br>
-                <select class="form-control text-info" name="bookingID" style="width: 450px" onclick="getInfo(this.value)">
+                <label for="bookingID" class="text-dark ">Lịch khám:</label><br>
+                <select class="form-control text-info" name="bookingID" style="width: auto" onclick="getInfo(this.value)">
                     <?php
                         $schedules = $data['schedule'];
                         foreach( $schedules as $schedule){
@@ -22,14 +22,14 @@
                 </select>
             </div>
             <div class="form-group">
-                <label class="text-dark font-weight-bold"><?php echo $_SESSION['type'] == 'patient'? 'Kết quả khám:' :'Biến chứng:'?></label><br>
+                <label class="text-dark"><?php echo $_SESSION['type'] == 'patient'? 'Kết quả khám:' :'Biến chứng:'?></label><br>
                 
-                <textarea readonly class="form-control" rows="4" name="view" class="form-control" id="showInfo"></textarea>
+                <textarea readonly rows="4" name="view" class="form-control text-info" id="showInfo"></textarea>
             </div>
             <div class="form-group">
-                <label class="text-dark font-weight-bold"><?php echo $_SESSION['type'] == 'doctor'? 'Kết quả khám bệnh:' :'Biến chứng:'?></label><br>
+                <label class="text-dark "><?php echo $_SESSION['type'] == 'doctor'? 'Kết quả khám bệnh:' :'Biến chứng:'?></label><br>
                 
-                <textarea class="form-control" rows="4" name="<?php $content =$_SESSION['type'] == 'doctor' ? 'reportDetail' : 'complication'; echo $content ?>" class="form-control"></textarea>
+                <textarea class="form-control text-info" rows="4" name="<?php $content =$_SESSION['type'] == 'doctor' ? 'reportDetail' : 'complication'; echo $content ?>" class="form-control"></textarea>
             </div>
             <div class="form-group text-light">
                 <input class="btn btn-info btn-md" type="submit" name="complicant" value="Gửi">
